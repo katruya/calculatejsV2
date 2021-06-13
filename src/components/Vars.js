81ce0b4dd9a842831c5d3f3106a6d41aa6439e3c
katruya <kybryce4@gmail.com>
2021-06-12 21:30:05 -0500

    updated style.css and added regular var buttons

--- src/components/Vars.js
+++ src/components/Vars.js
@@ -3,13 +3,24 @@ import CalculatorBtn from "./CalculatorBtn.js";
 const Vars = function() {
     return `<div id="vars" class="col">
                 <div class="row">
-                    ${CalculatorBtn('X=', 'x', 'variable-btn')}
+                    ${CalculatorBtn('X', 'x', 'variable-btn')}
                 </div>
                 <div class="row">
-                    ${CalculatorBtn('Y= ', 'y', 'variable-btn')}
+                    ${CalculatorBtn('Y ', 'y', 'variable-btn')}
                 </div>
                 <div class="row">
-                    ${CalculatorBtn('Z=', 'z', 'variable-btn')}
+                    ${CalculatorBtn('Z', 'z', 'variable-btn')}
+                </div>
+            </div>
+            <div id="var-btns" class="col">
+                <div class="row">
+                    ${CalculatorBtn('X=', 'x-equal', 'variable-btn')}
+                </div>
+                <div class="row">
+                    ${CalculatorBtn('Y= ', 'y-equal', 'variable-btn')}
+                </div>
+                <div class="row">
+                    ${CalculatorBtn('Z=', 'z-equal', 'variable-btn')}
                 </div>
             </div>`;
 }
